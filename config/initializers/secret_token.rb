d2a87f332663b10759370d4236f9ffcb1bf5e8f2
Blurredbits <mmorris00@mac.com>
2013-07-24 10:27:12 -0600

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailYoutubeFriday::Application.config.secret_token = '87213dae2916120a65889b878044d81bed4a4aa0a41a2ef857371d873e1211a8e22153cabcf4ed54b21f41300e2f62960a0321f85f483fb909dc6ff15a4bfd52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailYoutubeFriday::Application.config.session_store :cookie_store, key: '_rail_youtube_friday_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailYoutubeFriday::Application.config.session_store :active_record_store
